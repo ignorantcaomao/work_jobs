83d3c20ac60b01fc8fd69389e6bfe00dc9cc4233
ignorantcaomao <caomaodjs@163.com>
2021-06-01 23:41:00 +0800

    完成任务的取数

--- jobs/youtrack.py
+++ jobs/youtrack.py
@@ -4,8 +4,11 @@
 # @Author  : djs
 # @FileName: youtrack
 # @Software: PyCharm
+import asyncio
+import aiohttp
 import json
 import requests
+import time
 from jobs.getConfig import conf
 from logs import logger
 
@@ -33,6 +36,7 @@ class Yourtrack():
             'Cookie': self.Cookie
         }
 
+    # 获取所有的项目
     def get_projects(self):
         try:
             url = base_url + '/api/admin/projects?fields=id,name,shortName'
@@ -44,7 +48,57 @@ class Yourtrack():
             return result
 
         except Exception as e:
-            print(e)
+            logger.error(e)
+
+    def get_workItems(self):
+        try:
+            url = base_url + '/api/workItems?fields=created,duration(presentation,minutes),author(name),creator(name),date,id'
+            body = requests.get(url=url, headers=self.__headers, timeout=int(self.timeout))
+            data = json.loads(body.text)
+            print(len(data))
+            for item in data:
+                created = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(item.get('created') // 1000))
+                author = item.get('author').get('name')
+                creator = item.get('creator').get('name')
+
+            # result = sorted(data, key=lambda x: x['id'], reverse=True)
+            # logger.info(result)
+        except Exception as e:
+            logger.error(e)
+
+    def get_issue_by_project(self):
+        try:
+            url = base_url + "/api/admin/projects/17CJY003/issues?fields=id,summary,customFields(id,name,value(fullName,id,minutes,name,presentation,text))&$skip=0&$top=50"
+            body = requests.get(url=url, headers=self.__headers)
+            data = json.loads(body.text)
+            print(len(data))
+            # for item in data:
+            #     created = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(item.get('created')//1000))
+            #     author = item.get('author').get('name')
+            #     creator = item.get('creator').get('name')
+
+            # result = sorted(data, key=lambda x: x['id'], reverse=True)
+            # logger.info(result)
+        except Exception as e:
+            logger.error(e)
+
+    async def async_issue_by_project(self):
+        try:
+            url = base_url + "/api/admin/projects/17CJY003/issues?fields=id,summary,customFields(id,name,value(fullName,id,minutes,name,presentation,text))&$skip=0&$top=5000"
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url=url, headers=self.__headers) as res:
+                    resp = await res.text()
+                    data = json.loads(resp)
+                    for item in data:
+                        customFields = item.get('customFields')
+                        print([{k.get('name'): k.get('value').get('name')} for k in customFields])
+                        # Type = customFields[0].get('value').get('name')
+                        # 优先级 = customFields[1].get('value').get('name')
+                        # 状态 = customFields[1].get('value').get('name')
+                        break
+
+        except Exception as e:
+            logger.error(e)
 
 
 if __name__ == '__main__':
@@ -53,4 +107,10 @@ if __name__ == '__main__':
         conf.get('request', 'Cookie'),
         conf.get('request', 'timeout')
     )
-    youtrack.get_projects()
+    # youtrack.get_projects()
+    # youtrack.get_workItems()
+    # youtrack.get_issue_by_project()
+
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(youtrack.async_issue_by_project())
+    loop.run_until_complete(task)
